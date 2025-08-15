b2de23b9a0c6d240c984a42836e44938543fd29a
Mohamed Idrissi Alami <mohamedidrissialami22@gmail.com>
2025-08-15 11:05:26 +0100

    implement InsertAtBeginning()

--- clsDblLinkedList.h
+++ clsDblLinkedList.h
@@ -4,5 +4,37 @@ using namespace std;
 template <class T>
 class clsDblLinkedList
 {
+public:
+	class Node
+	{
+	public :
+		T Value;
+		Node* Prev, * Next;
+	};
+	Node* Head = nullptr;
+
+	void InsertAtBeginning(T Value)
+	{
+		Node* NewNode = new  Node();
+		NewNode->Value = Value;
+		NewNode->Prev = nullptr;
+		NewNode->Next = Head;
+		if (Head != nullptr)
+		{
+			Head->Prev = NewNode;
+		}
+		Head = NewNode;
+	}
+
+	void PrintList()
+	{
+		Node* Current = Head;
+		while (Current != nullptr)
+		{
+			cout << Current->Value << " ";
+			Current = Current->Next;
+		}
+	}
+
 };
 
--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 int main()
 {
 	clsDblLinkedList<int> MyList;
+	MyList.InsertAtBeginning(5);
+	MyList.InsertAtBeginning(4);
+	MyList.InsertAtBeginning(3);
+	MyList.InsertAtBeginning(2);
+	MyList.InsertAtBeginning(1);
+
+	cout << "\nLinked list content : \n";
+	MyList.PrintList();
 	system("pause>0");
 	return 0;
 }
